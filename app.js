0659669529286756fcc5eed97357577008bd99e6
durdona <123@.ru>
2023-09-14 22:48:56 +0500

    final

---
+++ app.js
@@ -0,0 +1,43 @@
+/* //Задание 1
+
+var imya = prompt('Введите имя')
+console.log('Меня зовут ' + imya)
+
+var vozrast = +prompt('Введите возраст')
+console.log('Мне ' + vozrast + " лет")
+
+var zadanie = ('  РЕШИТЕ ПРИМЕР  ')
+var primer1= +prompt( zadanie + ' 5+5=?  ')
+var a = 10
+console.log('5+5=' + a   + ' ваш отвeт: ' + primer1  );
+
+var primer2= +prompt( zadanie  +  '  545+1234=?  ')
+var otvettext = '  Bаш отвeт:  '
+var b = 545+1234
+console.log(' 545+1234=  ' + b+otvettext+primer2  );
+
+var primer3= +prompt( zadanie  + '  543+(123-12)=? ')
+var c = 543+(123-12)
+console.log(' 543+(123-12)= '+ c+otvettext+primer3);
+
+var primer4= +prompt( zadanie  + '  125/5=? ')
+var d = 125/5
+console.log('  125/5= ' + d+otvettext+primer4);
+
+var primer5= +prompt( zadanie  + '  34*23=? ')
+var e = 34*23
+console.log('  34*23= ' + e+otvettext+primer5);
+
+var zadanie2='  Извлечь остаток '
+var primer6= +prompt( zadanie2  + '  456%32=? ')
+var f = 456%32
+console.log('  456%32= ' + ' остаток '+f+otvettext+primer6)
+ */
+
+//Задание 2
+
+/* var x= +prompt('53')
+var y= +prompt('24')
+var z= +prompt('36')
+var res = (x+y+z) / 3
+console.log(res); */
